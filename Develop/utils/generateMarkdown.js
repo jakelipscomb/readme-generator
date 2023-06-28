746d006a2c700261e38c69b651e0c29672904e7b
jakelipscomb <jlipscombtx95@gmail.com>
2023-06-27 21:51:42 -0500

    added required packages for assignment along with questions to be asked.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,42 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) 
+  if (license !== 'No License') {
+    return ``;
+  }
+
+  return "";
+
+  const licenseImg = {
+
+  // apache = 'Apache 2.0'
+  // apachePath = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+
+  // mit = 'MIT'
+  // mitPath = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+
+  // boost = 'Boost Software'
+  // boostPath = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]'
+
+  // gnu = 'General Public v3.0'
+  // gnuPath = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]'
+
+  // mozilla = 'Mozilla Public 2.0'
+  // mozillaPath = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+
+
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  boostLink = '(https://www.boost.org/LICENSE_1_0.txt)'
+
+}
+
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
