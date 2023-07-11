8a7f638b80ca83925dc9c9bd495092f64a81f913
jakelipscomb <jlipscombtx95@gmail.com>
2023-07-10 22:53:43 -0500

    added functionality to index.js to run inquirer and create a README.md. Also added the basics for what a README.md would look like when the node index.js command is run in the command line to generate a functional readme doc.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,51 +1,57 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) 
-  if (license !== 'No License') {
-    return ``;
+function renderLicenseBadge(license) {
+  if (license !== "No License") {
+    return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
   }
-
   return "";
-
-  const licenseImg = {
-
-  // apache = 'Apache 2.0'
-  // apachePath = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-
-  // mit = 'MIT'
-  // mitPath = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
-
-  // boost = 'Boost Software'
-  // boostPath = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]'
-
-  // gnu = 'General Public v3.0'
-  // gnuPath = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]'
-
-  // mozilla = 'Mozilla Public 2.0'
-  // mozillaPath = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
-
-
 }
-
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Function that returns the license link
 function renderLicenseLink(license) {
-  boostLink = '(https://www.boost.org/LICENSE_1_0.txt)'
-
+  if (license !== "No License") {
+    return `\n [License](#license) \n`;
+  }
+  return "";
 }
 
-
-
-
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "No License") {
+    return `## License
+      This Project is using the ${license} license.`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data.title} by ${data.username}
+  ${renderLicenseBadge(data.licenses)}
+  ## Table of Contents
+  * [Description](#description)
+  * [Requirements](#requirements)
+  * [Usage](#usage)
+  * [Contact-Info](#contact-info)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+${renderLicenseLink(data.licenses)}
+## Description
+${data.description}
+## Requirements
+${data.require}
+## Usage
+${data.languages}
+## Contact-Info
+* Username ${data.username}(https://github.com/${data.username}/)
+* Email ${data.email}
+## Contributors
+${data.contributors}
+## Testing
+\n
+${data.testing}
+\n
+${renderLicenseSection(data.license)}
 `;
 }
 
---
+++ index.js
@@ -0,0 +1,76 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./Develop/utils/generateMarkdown")
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Git username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description on the purpose and functionality of your project.',
+    },
+    {
+        type: 'checkbox',
+        name: 'licenses',
+        message: 'What license are you using for your project?',
+        choices: ['Apache_2.0', 'GPLv3', 'MIT', 'Boost_1.0', 'MPL_2.0', 'No License'],
+    },
+    {
+        type: 'input',
+        name: 'require',
+        message: 'List any project dependencies that are being used.'
+    },
+    {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What langauges are utilized in this project?',
+        choices: ['html', 'CSS', 'JavaScript', 'MySQL']
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'List any contributors that worked on this project using GitHub usernames. If none, type "none".'
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'Provide a short walkthrough of required tests used for this project. If none, type "none".'
+    },
+
+];
+
+//Function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((res) => {
+        console.log("Creating README.md file...");
+        writeToFile("./Develop/docs/README.md", generateMarkdown(res))
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+init();
